6c89bbe84ba38b4bdfc3849479b664e48483cd02
Arash Altafi <arashaltafi1377@gmail.com>
2025-04-04 02:17:33 +0430

    a

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.experiments = { asyncWebAssembly: true };
+    return config;
+  },
 };
 
 export default nextConfig;
